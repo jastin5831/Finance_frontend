0a1745f39eb307ea00da55b7683c236b660cb273
Jastin <jastinmax999@gmail.com>
2024-12-29 03:12:59 +0900

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 // config
 import { HOST_API } from 'src/config-global';
 
-// ----------------------------------------------------------------------
-
 const axiosInstance = axios.create({ baseURL: HOST_API });
 
 axiosInstance.interceptors.response.use(
